45a911af9e302bcd43d4592f4be8f087c378153f
laurasherwin <lsherwin@unomaha.edu>
2020-07-11 17:19:17 -0500

    First commit

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="skills">
+       <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
@@ -9,7 +8,7 @@
 import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
+  name: 'Skills',
   components: {
     HelloWorld
   }
@@ -17,12 +16,22 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@import url('https://fonts.googleapis.com/css?family=Montserrat:400,700');
+
+body {
+  background-color: #EEEEEE;
+  font-family: 'Montserrat', sans-serif;
+  display: grid;
+  grid-template-rows: auto;
+  justify-items: center;
+  align-items: center;
 }
+body, html {
+  margin: 0;
+  height: 100%;
+}
+#app {
+    width: 50%;
+}
+
 </style>
